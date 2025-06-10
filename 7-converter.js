a5d5634022515a0bb7cb446ccf98b628bf1644a6
Andrey <admin@sakhparfum.ru>
2025-06-10 12:58:16 +1100

    HW_7

---
+++ 7-converter.js
@@ -0,0 +1,45 @@
+
+function converter (sum, currency_1, currency_2){
+    const USDtoRUB = 80;
+    const RUBtoUSD = 1/80;
+    const USDtoEURO = 0.9;
+    const EUROtoUSD = 1.11;
+    const RUBtoEURO = 1/90;
+    const EUROtoRUB = 90;
+    let curs = null;
+    if (currency_1 == "USD"){
+        switch (currency_2){
+            case "RUB":
+                curs = USDtoRUB;
+                break;
+            case "EURO":
+                curs = USDtoEURO;
+                break;
+        } 
+    } else if (currency_1 == "EURO"){
+            switch (currency_2){
+                case "USD":
+                    curs = EUROtoUSD;
+                    break;
+                case "RUB":
+                    curs = EUROtoRUB;
+                    break;
+            } 
+        } else if (currency_1 == "RUB"){
+                switch (currency_2){
+                    case "USD":
+                        curs = RUBtoUSD;
+                        break;
+                    case "EURO":
+                        curs = RUBtoEURO;
+                        break;
+                } 
+            } else {
+                    curs = null;
+                }
+
+    return sum * curs;
+            }
+    
+console.log(converter(1000, "RUB", "EURO"));
+
