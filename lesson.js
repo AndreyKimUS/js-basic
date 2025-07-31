62196040549a69d7561469e870f0b34e0faaf977
Andrey Kim <admin@sakhparfum.ru>
2025-07-31 23:56:21 +0300

    Homework DOM

--- lesson.js
+++ lesson.js
@@ -1,3 +1,57 @@
+'use strict'
+
+const userInfo = {
+    balance: 0,
+    operations: 0,
+    increse(sum){
+        this.balance += sum;
+        this.operations++;
+    }
+}
+
+function newObj(user){
+    return function(){
+        
+    }
+
+// function removePassword(reset){
+//     if(reset){
+//         this.password = undefined;      
+//    } else {
+//         this.password = '1';
+//    }
+// };
+
+// const user = {
+//     login: '23423',
+//     password: 'Tristan',
+// };
+
+// const removePasswordUser = removePassword.bind(user, true);
+// removePasswordUser();
+// console.log(user);
+
+
+// const company = {
+//     name: 'OOO AGRO',
+//     employees: [
+//         {
+//             name: 'Sveta'
+//         }
+// ],
+//     CEO: {
+//         name: 'Vasya',
+//     },
+//     getName: function (){
+//         return this.name;
+//     },
+
+
+//     }
+
+// }
+
+
 /* const arr = ["!", "JS", "Love", "I"];
 const arr2 = [];
 for (i = arr.length; i > 0; i-- ){
@@ -11,16 +65,16 @@ console.log(str); */
 // console.log(pow (2)(3));
 
 
-const arr = [2, 3, 8, 10, 1, 4];
+// const arr = [2, 3, 8, 10, 1, 4];
 
-function some(array, elem){
-    for (el of array){
-        if (el == elem) {
-            return true;
-            break;
-        }
-    }
-    return false;
-}
+// function some(array, elem){
+//     for (el of array){
+//         if (el == elem) {
+//             return true;
+//             break;
+//         }
+//     }
+//     return false;
+// }
 
-console.log(some(arr, 11));
+// console.log(some(arr, 11));
